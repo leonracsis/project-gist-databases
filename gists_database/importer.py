8b815b00726ef7d237a75d8d479cc1edcefef3d4
noels <noelsiscar@protonmail.com>
2019-06-18 11:56:07 +0000

    B34 Class Noel S 6/18

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,69 @@
 import requests
+import json
+import sqlite3
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    
+    print(username)
+    req_str = 'https://api.github.com/users/{}/gists'.format(username)
+    resp = requests.get(req_str)
+    print (resp.status_code)
+    if resp.status_code==404:
+        raise requests.exceptions.HTTPError        
+        
+    gists_list = resp.json()
+    db.executescript("""
+    DROP TABLE if exists gists;
+    CREATE TABLE gists (
+      id INTEGER PRIMARY KEY autoincrement,
+      github_id TEXT NOT NULL,
+      html_url TEXT NOT NULL,
+
+      git_pull_url TEXT NOT NULL,
+      git_push_url TEXT NOT NULL,
+
+      commits_url TEXT NOT NULL,
+      forks_url TEXT NOT NULL,
+
+      public BOOLEAN NOT NULL,
+
+      created_at DATETIME NOT NULL,
+      updated_at DATETIME NOT NULL,
+
+      comments INTEGER NOT NULL,
+      comments_url TEXT NOT NULL
+    );
+    """)
+   
+    insert_str = """
+    INSERT INTO gists 
+        (github_id, 
+        html_url, 
+        git_pull_url, 
+        git_push_url, 
+        commits_url, 
+        forks_url, 
+        public, 
+        created_at, 
+        updated_at, 
+        comments, 
+        comments_url)
+    VALUES (?,?,?,?,?,?,?,?,?,?,?)
+    """
+
+    for gist_rec in gists_list:       
+        db.execute(insert_str,                  
+                  (gist_rec['id'],
+                   gist_rec['html_url'],
+                   gist_rec['git_pull_url'],
+                   gist_rec['git_push_url'],
+                   gist_rec['commits_url'],
+                   gist_rec['forks_url'],
+                   gist_rec['public'],
+                   gist_rec['created_at'],
+                   gist_rec['updated_at'],
+                   gist_rec['comments'],
+                   gist_rec['comments_url'] ))
+
+    if commit:
+        db.commit()
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,43 @@
 from .models import Gist
+from datetime import datetime
 
 def search_gists(db_connection, **kwargs):
-    pass
+
+    parm_github_id=None
+    parm_created_at=None
+    
+    for key, value in kwargs.items():        
+        if key=='github_id':
+            parm_github_id=value
+        if key=='created_at':
+            parm_created_at=value
+            
+    sel_str1 = 'SELECT *  FROM gists'
+    
+    if parm_github_id and parm_created_at:
+        sel_str2 = ' WHERE github_id = ? and created_at = ?'
+        select_str = sel_str1 + sel_str2
+        result=db_connection.execute(select_str,(parm_github_id, parm_created_at))
+        
+    if parm_github_id and parm_created_at is None:
+        sel_str2 = " WHERE github_id = '{}'"
+        select_str = sel_str1 + sel_str2.format(parm_github_id)
+        result=db_connection.execute(select_str)        
+        
+        
+    if parm_github_id is None and parm_created_at:
+        sel_str2 = " WHERE created_at = '{}Z'"
+        select_str = sel_str1 + sel_str2.format(datetime.isoformat(parm_created_at))
+        
+        result=db_connection.execute(select_str)        
+
+    if parm_github_id is None and parm_created_at is None:
+        result=db_connection.execute(sel_str1)        
+
+    result_list=[]
+    for rec in result.fetchall():
+        result_class = Gist(rec)
+        result_list.append(result_class)
+        
+    return result_list
+    
